6777751cdb1f1dc6bbabe35fac25cc887b02dd73
onurinc <onur.i@hotmail.nl>
2021-12-02 23:54:24 +0100

    Added a User entity & updated the database

--- MR.DataAccessLayer/Context/MovieReviewerContext.cs
+++ MR.DataAccessLayer/Context/MovieReviewerContext.cs
@@ -1,21 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MR.DataAccessLayer.Entities;
 
 namespace MR.DataAccessLayer.Context
 {
-    public class MovieReviewerContext:DbContext
+    public class MovieReviewerContext: IdentityDbContext
     {
-        public MovieReviewerContext(DbContextOptions<MovieReviewerContext> options) 
-            : base(options)
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public MovieReviewerContext(DbContextOptions<MovieReviewerContext> options) : base(options)
         {
 
         }
 
-        public DbSet<Entities.Comment> Comments { get; set; }
 
     }
 }
---
+++ MR.DataAccessLayer/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MR.DataAccessLayer.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public int Status { get; set; } = 1;
+
+    }
+}
